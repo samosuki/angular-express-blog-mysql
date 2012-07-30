3194a62d53612a226437507709d2238c89964e5d
Samora Reid <samora.m.reid@gmail.com>
2012-07-30 06:15:25 -0500

    Get all, Get Specific, Add Post working. Edit and Delete Post not.

--- app.js
+++ app.js
@@ -4,8 +4,19 @@
  */
 
 var express = require('express'),
-  routes = require('./routes'),
-  api = require('./routes/api');
+    routes = require('./routes'),
+    _ = require("underscore"),
+    api = require('./routes/api');
+
+var mysql = require('mysql');
+var database = 'blogposts';
+var client = mysql.createClient({
+  user: 'root',
+  password: 'password',
+  host: 'localhost',
+  port: '',
+  database: 'blogposts'
+});
 
 var app = module.exports = express.createServer();
 
@@ -38,7 +49,88 @@ app.get('/partials/:name', routes.partials);
 
 // JSON API
 
-app.get('/api/name', api.name);
+// GET ALL
+
+app.get('/api/posts', function (req, res) {
+
+var posts = [];
+
+  client.query('SELECT * FROM posts',
+  function (err, results, fields) {
+    if (err) {
+      throw err;
+    }
+    console.log(results);
+    //var deposts = _.first(results, [1]);
+    //res.send(deposts);
+    //console.log(fields);
+    //var json = JSON.stringify(results);
+    //res.json(results);
+    res.send(results);
+    //client.end();
+    }
+  );
+});
+
+// GET
+
+app.get('/api/posts/:pid', function (req, res) {
+  var pid = req.params.pid;
+  client.query('SELECT * FROM posts WHERE id = ?', [pid],
+  function (err, results, fields) {
+    if (err) {
+      throw err;
+    }
+    console.log("--- Specific Post ---");
+    console.log(results);
+    //var deposts = _.first(results, [1]);
+    var result = JSON.stringify(results);
+    res.end(result);
+
+    if (results) {
+      //res.send(results);
+      console.log("Results exists");
+    } else {
+      res.json(false);
+      console.log("Nothing Returned");
+    }
+    
+    }
+  );
+});
+
+// POST
+
+app.post('/api/post', function (req, res) {
+
+  var ptitle = req.body.title;
+  var pposts = req.body.text;
+  client.query('INSERT INTO posts SET title = ?, posts = ?', [ptitle, pposts]);
+    res.send(req.body);
+});
+
+// PUT
+
+app.put('/api/posts/:pid', function(req, res) {
+  var ptitle = req.body.title;
+  var pposts = req.body.text;
+  var pid = req.params.pid;
+  client.query('UPDATE posts SET title = ?, posts = ? WHERE id = ?', [ptitle, pposts, pid],
+  function (err, results, fields) {
+    if (err) {
+      throw err;
+    }
+    res.json(results);
+    //req.body = results;
+    console.log(results);
+    res.send(req.body);
+    
+    }
+  );
+});
+
+
+app.delete('/api/post/:id', api.deletePost);
 
 // redirect all others to the index (HTML5 history)
 app.get('*', routes.index);
--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,21 +1,69 @@
-'use strict';
-
-/* Controllers */
-
-function AppCtrl($scope, $http) {
-  $http({method: 'GET', url: '/json/name'}).
-  success(function(data, status, headers, config) {
-    $scope.name = data.name;
-  }).
-  error(function(data, status, headers, config) {
-    $scope.name = 'Error!'
-  });
+function IndexCtrl($scope, $http) {
+  $http.get('/api/posts').
+    success(function(data) {
+      $scope.posts = data;
+      console.log(data);
+    });
 }
-
-function MyCtrl1() {}
-MyCtrl1.$inject = [];
-
-
-function MyCtrl2() {
+ 
+function AddPostCtrl($scope, $http, $location) {
+  //$scope.form = {};
+  $scope.submitPost = function () {
+    $http.post('/api/post', $scope.form).
+      success(function(data) {
+        $location.path('/');
+      });
+  };
 }
-MyCtrl2.$inject = [];
+ 
+function ReadPostCtrl($scope, $http, $routeParams) {
+  $http.get('/api/posts/' + $routeParams.pid).
+    success(function(data, status) {
+      $scope.status = status;
+      $scope.posts = data;
+      
+    }).
+    error(function(data, status) {
+      $scope.data = data || "Request failed";
+      $scope.status = status;
+    });
+    //return;
+}
+ 
+function EditPostCtrl($scope, $http, $location, $routeParams) {
+ // $scope.form = {};
+  $http.get('/api/posts/' + $routeParams.pid).
+    success(function(data, status) {
+      $scope.status = status;
+      //$scope.form = data;
+      editText = data[0];
+      $scope.editForm = editText;
+      $scope.posts = data;
+      console.log(data);
+    });
+    
+  $scope.editPost = function () {
+    $http.put('/api/post/' + $routeParams.pid, $scope.form).
+      success(function(data) {
+        $location.url('/readPost/' + $routeParams.pid);
+      });
+  };
+}
+ 
+function DeletePostCtrl($scope, $http, $location, $routeParams) {
+  $http.get('/api/post/' + $routeParams.id).
+    success(function(data) {
+      $scope.post = data.post;
+    });
+    
+  $scope.deletePost = function () {
+    $http.delete('/api/post/' + $routeParams.id).
+      success(function(data) {
+        $location.url('/');
+      });
+  };
+  
+  $scope.home = function () {
+    $location.url('/');
+  };
+}
